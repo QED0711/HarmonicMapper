84341ec7cf7f051cc97fba0a20ea2cc1a83ffb59
QED0711 <qdizon@gmail.com>
2018-07-24 07:32:16 -0400

    verify tranposition working

--- assets/js/mouseOver.js
+++ assets/js/mouseOver.js
@@ -4,5 +4,5 @@ let schemeInput = $("#intervalScheme");
 console.log(schemeInput);
 
 schemeInput[0].onmouseover = () => {
-    // make a popup gox that tells the user how to use this field
+    // make a popup box that tells the user how to use this field
 }
